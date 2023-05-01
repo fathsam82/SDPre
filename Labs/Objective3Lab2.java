66a8cb4ef2b1118e7fab7aba5c2b4ca4690b94b2
fathsam82 <fathsam82@gmail.com>
2023-05-01 02:24:01 -0600

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+	public static void main (String[] args) {
+		double side1 = (12 * 12);
+		double side2 = (15 * 15);
+		double hypotenuse = Math.sqrt(side1 + side2);
+
+		System.out.println("The hypotenuse of a triangle with side 12.0 and 15.0 is" + " " + hypotenuse);
+
+	}
+}
