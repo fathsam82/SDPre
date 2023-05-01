e293168774cc0115aa3d4bbab4b4c6301e0af8bf
fathsam82 <fathsam82@gmail.com>
2023-05-01 00:26:40 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Sam Fath";
+		int age = 28;
+		String state = "Ohio";
+
+		System.out.println("Name:" + " " + name + " " + ("Age:" + " " + age + " " + ("State:" + " " + state)));
+	}
+}
