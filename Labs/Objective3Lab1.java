1ddcccd16c31c5fc436bdad6482a0689fbcbf37f
fathsam82 <fathsam82@gmail.com>
2023-05-01 01:33:49 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+	public static void main (String[] args) {
+		double num1 = 4;
+		double num2 = 7;
+		double num3 = 14;
+		double average = ((num1 + num2 + num3) / 3);
+
+		System.out.println(average);
+
+	}
+}
