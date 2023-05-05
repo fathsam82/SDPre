721a7051ac45517ba2a3ce2713be0b51e251342d
fathsam82 <fathsam82@gmail.com>
2023-05-05 11:38:33 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+
+		for (int a=1; a<=10; a++) {
+			System.out.println(a);
+		}
+		for (int x=10; x>=1; x--) {
+			System.out.println(x);
+		}
+	}
+}
